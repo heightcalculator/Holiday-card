4d40dffc571e98ceaf78c31ef6d7ba8c35e643eb
heightcalculator <91100197+heightcalculator@users.noreply.github.com>
2022-12-15 14:26:26 -0500

    Add files via upload

---
+++ individuals/2022/index.js
@@ -0,0 +1,22 @@
+function hideStuff(id) {
+  document.getElementById(id).style.display = 'none';
+}
+const date = new Date();
+if((date.getDate() < 25 && date.getMonth() + 1 === 12 ) && date.getYear()+1900 === 2022){
+      hideStuff("everything");
+      alert("Sorry, Nothing available yet. Check back on Christmas Day :)")
+      }
+
+document.body.appendChild(template.content);
+   if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+  console.log("Mobile")
+} else {
+  console.log("Desktop")
+}
+   console.log((date.getMonth() + 1)+"/"+date.getDate()+"/"+(date.getYear()+1900))
+document.getElementById("reply").onclick=function(){
+   if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+  window.location.href='sms:+1-770-569-3987'
+} else {
+  window.location.href='mailto:aarush@aarushmagic.com'
+}}
